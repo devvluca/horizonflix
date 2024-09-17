8a7836bbaa8bdabbdea6ef7681ac256a8e07fc21
Luca Aguiar <lucanobre1@gmail.com>
2024-09-17 19:05:58 -0300

    commit

--- src/App.js
+++ src/App.js
@@ -4,23 +4,23 @@ import './App.scss';
 
 import { BrowserRouter, Route } from 'react-router-dom';
 
-import Header from './components/header/Header'
-import Footer from './components/footer/Footer'
+import Header from './components/header/Header';
+import Footer from './components/footer/Footer';
 
-import Routes from './config/Routes'
+import Routes from './config/Routes';
 
 function App() {
-  return (
-    <BrowserRouter>
-        <Route render={props => (
-          <>
-              <Header {...props}/>
-              <Routes/>
-              <Footer/>          
-          </>
-        )}/>
-    </BrowserRouter>
-  );
+    return (
+        <BrowserRouter>
+            <Route render={props => (
+                <>
+                    <Header {...props}/>
+                    <Routes/>
+                    <Footer/>
+                </>
+            )}/>
+        </BrowserRouter>
+    );
 }
 
 export default App;
