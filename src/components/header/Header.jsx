9e185f1c114d0ff12ccbd64abee6e69d5d78f34a
Luca Aguiar <lucanobre1@gmail.com>
2024-09-24 19:08:45 -0300

    button

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,4 +1,5 @@
 import React, { useRef, useEffect } from 'react';
+
 import { Link, useLocation } from 'react-router-dom';
 
 import './header.scss';
@@ -25,41 +26,27 @@ const Header = () => {
     const { pathname } = useLocation();
     const headerRef = useRef(null);
 
-    const active = headerNav.findIndex(e => {
-        // Verificar Home de forma exata
-        if (e.path === '/') return pathname === e.path;
-        // Verificar as outras rotas usando includes para garantir que seja parte da URL
-        return pathname.includes(e.path);
-    });
+    const active = headerNav.findIndex(e => e.path === pathname);
 
     useEffect(() => {
-        // Função para encolher o header ao rolar
         const shrinkHeader = () => {
             if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
                 headerRef.current.classList.add('shrink');
             } else {
                 headerRef.current.classList.remove('shrink');
             }
-        };
-        // Adicionar event listener para rolagem
+        }
         window.addEventListener('scroll', shrinkHeader);
-
-        // Remover event listener quando o componente for desmontado
         return () => {
             window.removeEventListener('scroll', shrinkHeader);
         };
     }, []);
 
-    useEffect(() => {
-        // Executa sempre que o pathname mudar para atualizar o componente visualmente
-        // Isso garante que a barra vermelha mude quando a rota mudar
-    }, [pathname]);
-
     return (
         <div ref={headerRef} className="header">
             <div className="header__wrap container">
                 <div className="logo">
-                    <img src={logo} alt="StarCine Logo" />
+                    <img src={logo} alt="" />
                     <Link to="/">StarCine</Link>
                 </div>
                 <ul className="header__nav">
